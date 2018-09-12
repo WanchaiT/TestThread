3ac06e4cedf91053eccc4cbf5f2f94de69ea1ea0
Wanchai Thiantanawat <wanchairoot@WanMac.local>
2018-09-12 11:18:34 +0700

    j

--- ThreadDemo.java
+++ ThreadDemo.java
@@ -1,15 +1,13 @@
 import java.util.concurrent.*;
 public class ThreadDemo {
     public static void main(String[] args) {
-        /*ThreadPrintNum thread0 = new ThreadPrintNum(10);
-        thread0.start();*/
-        ThreadPrintChar thread1 = new ThreadPrintChar('A', 100);
-        ThreadPrintChar thread2 = new ThreadPrintChar('B', 100);
-        ThreadPrintNum thread3 = new ThreadPrintNum(100);
-
-        //thread1.setPriority(5);
-        thread1.start();
-        thread2.start();
-        thread3.start();
+//ExecutorService executor = Executors.newCachedThreadPool();
+// try changing parameter from 3 to 1 and see what happen
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        executor.execute(new ThreadPrintChar('a',100));
+        executor.execute(new ThreadPrintChar('B',100));
+        executor.execute(new ThreadPrintNum(100));
+        executor.shutdown();
+        
     }
 }
